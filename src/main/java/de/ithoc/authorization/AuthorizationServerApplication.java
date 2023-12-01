8b337680e54ceba11cc0ab8c42d6780ac42b4925
Oli Hock <oliver.hock@gmail.com>
2023-12-01 19:15:48 +0100

    feat: Add ApiKeyRepository and ApiKeyEntity
    This commit adds the `ApiKeyRepository` interface and the `ApiKeyEntity` class to the `persistance` package. The `ApiKeyRepository` extends the `JpaRepository` interface and is responsible for CRUD operations on the `ApiKeyEntity` objects. The `ApiKeyEntity` class represents an API key and contains fields for the ID, API key value, client, and audience.

--- src/main/java/de/ithoc/authorization/AuthorizationServerApplication.java
+++ src/main/java/de/ithoc/authorization/AuthorizationServerApplication.java
@@ -1,14 +1,13 @@
 package de.ithoc.authorization;
 
-import de.ithoc.authorization.persistance.AudienceEntity;
-import de.ithoc.authorization.persistance.AudienceRepository;
-import de.ithoc.authorization.persistance.ClientEntity;
-import de.ithoc.authorization.persistance.ClientRepository;
+import de.ithoc.authorization.persistance.*;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCrypt;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class AuthorizationServerApplication {
@@ -18,10 +17,14 @@ public class AuthorizationServerApplication {
 
     private final AudienceRepository audienceRepository;
     private final ClientRepository clientRepository;
+    private final ApiKeyRepository apiKeyRepository;
 
-    public AuthorizationServerApplication(AudienceRepository audienceRepository, ClientRepository clientRepository) {
+    public AuthorizationServerApplication(AudienceRepository audienceRepository,
+                                          ClientRepository clientRepository,
+                                          ApiKeyRepository apiKeyRepository) {
         this.audienceRepository = audienceRepository;
         this.clientRepository = clientRepository;
+        this.apiKeyRepository = apiKeyRepository;
     }
 
     public static void main(String[] args) {
@@ -40,9 +43,18 @@ public class AuthorizationServerApplication {
         createClient("team-2", "SlVg0iGOhEXkOOZwlvw6", audienceEntity);
         createClient("team-3", "SlVg0iGOhEXkOOZwlvw6", audienceEntity);
         createClient("team-4", "SlVg0iGOhEXkOOZwlvw6", audienceEntity);
+
+        ClientEntity app1 = createClient("app-1", "SlVg0iGOhEXkOOZwlvw6", audienceEntity);
+
+        UUID uuid = UUID.fromString("d750c51a-d354-4e6d-ad50-b532946d3d11");
+        ApiKeyEntity apiKeyEntity = new ApiKeyEntity();
+        apiKeyEntity.setApiKey(uuid);
+        apiKeyEntity.setClient(app1);
+        apiKeyEntity.setAudience(audienceEntity);
+        apiKeyRepository.save(apiKeyEntity);
     }
 
-    private void createClient(String clientId,
+    private ClientEntity createClient(String clientId,
                               @SuppressWarnings("SameParameterValue") String clientSecret,
                               AudienceEntity audienceEntity) {
         ClientEntity clientEntity = new ClientEntity();
@@ -51,6 +63,8 @@ public class AuthorizationServerApplication {
         clientRepository.save(clientEntity);
         clientEntity.getAudiences().add(audienceEntity);
         clientRepository.save(clientEntity);
+
+        return clientEntity;
     }
 
 }
---
+++ src/main/java/de/ithoc/authorization/persistance/ApiKeyEntity.java
@@ -0,0 +1,26 @@
+package de.ithoc.authorization.persistance;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity(name = "api_key")
+public class ApiKeyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private UUID apiKey;
+
+    @OneToOne
+    private ClientEntity client;
+
+    @OneToOne
+    private AudienceEntity audience;
+
+}
