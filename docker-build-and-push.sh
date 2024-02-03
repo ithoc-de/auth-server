ee85c08057a994620f9cbfb76af22fa879631646
Oli Hock <oliver.hock@gmail.com>
2024-02-03 12:40:23 +0100

    chore: update docker images and add docker-compose script
    In this commit, the docker images for the auth-server-backend have been updated to version 1.1.0. Additionally, a new docker-compose script has been added to the repository, which allows for easier deployment of the containers.
    
    Changes:
    - Updated the docker build command to use the new image version 1.1.0.
    - Updated the docker push command to use the new image version 1.1.0.
    - Added a new .dockerignore file to exclude unnecessary files and directories from the docker build.
    - Added a new docker-deploy-container.sh script to simplify container deployment using docker-compose.
    
    Affected files:
    - docker-build-and-push.sh
    - .dockerignore
    - docker-deploy-container.sh

--- docker-build-and-push.sh
+++ docker-build-and-push.sh
@@ -1,3 +1,3 @@
-docker build -t olihock/auth-server-backend:1.0.0 .
+docker build -t olihock/auth-server-backend:1.1.0 .
 docker login
-docker push olihock/auth-server-backend:1.0.0
+docker push olihock/auth-server-backend:1.1.0
---
+++ docker-deploy-container.sh
@@ -0,0 +1,4 @@
+docker compose down
+docker login
+docker pull olihock/auth-server:1.1.0
+docker docker compose up -d
